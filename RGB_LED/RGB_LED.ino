aaf1a336ac11f0b79ee5489dd8892b99d2f5bf32
Arjun Hegde <arjunmeega28@gmail.com>
2022-04-04 11:49:00 +0530

    Add files via upload

---
+++ RGB_LED/RGB_LED.ino
@@ -0,0 +1,44 @@
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(9,OUTPUT);
+ pinMode(10,OUTPUT);
+ pinMode(11,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(9,LOW);
+ digitalWrite(10,LOW);
+ digitalWrite(11,LOW);
+ delay(1000);
+ digitalWrite(9,LOW);
+ digitalWrite(10,LOW);
+ digitalWrite(11,HIGH;
+ delay(1000);
+ digitalWrite(9,LOW);
+ digitalWrite(10,HIGH);
+ digitalWrite(11,LOW);
+ delay(1000);
+ digitalWrite(9,LOW);
+ digitalWrite(10,HIGH);
+ digitalWrite(11,HIGH);
+ delay(1000);
+ digitalWrite(9,HIGH);
+ digitalWrite(10,LOW);
+ digitalWrite(11,LOW);
+ delay(1000);
+ digitalWrite(9,HIGH);
+ digitalWrite(10,LOW);
+ digitalWrite(11,HIGH);
+ delay(1000);
+ digitalWrite(9,HIGH);
+ digitalWrite(10,HIGH);
+ digitalWrite(11,LOW);
+ delay(1000);
+digitalWrite(9,HIGH);
+ digitalWrite(10,HIGH);
+ digitalWrite(11,HIGH);
+ delay(1000);
+ 
+ 
+}
