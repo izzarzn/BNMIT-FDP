4d64aaa6b6393314335e8dcc66718a8443c0e487
Arjun Hegde <arjunmeega28@gmail.com>
2022-04-04 11:48:25 +0530

    Add files via upload

---
+++ Blink_LED/Blink_LED.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+     pinMode(11,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+      digitalWrite(11,HIGH);
+      delay(1000);
+      digitalWrite(11,LOW);
+      delay(1000);
+      
+}
